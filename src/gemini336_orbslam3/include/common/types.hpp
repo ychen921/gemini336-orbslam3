d0e0de99b4870d6b9a247cc0c0dd47c5b70325d7
ychen921 <steven88669@gmail.com>
2026-09-18 14:27:04 +0800

    feat: add IMU measurement data type

--- src/gemini336_orbslam3/include/common/types.hpp
+++ src/gemini336_orbslam3/include/common/types.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <Eigen/Core>
 #include <opencv2/core.hpp>
 
 namespace gemini336_orbslam3
@@ -9,5 +10,15 @@ struct StereoFrame
     cv::Mat left;
     cv::Mat right;
     double timestamp = 0.0;
+};
+
+struct ImuMeasurement
+{
+    // Seconds from the IMU message header stamp, on the same time basis as StereoFrame.
+    double timestamp = 0.0;
+
+    // Preserve the source IMU frame; no rotation or gravity removal is applied.
+    Eigen::Vector3f accel = Eigen::Vector3f::Zero();  // m/s^2
+    Eigen::Vector3f gyro = Eigen::Vector3f::Zero();   // rad/s
 };
 }
